e1a75fdd912d0ef18e115069800e5784a0627705
tails618 <asher.grossman@gmail.com>
2018-09-25 19:20:01 -0500

    Edited Document

---
+++ python.py
@@ -0,0 +1,13 @@
+class PirateNameGenerator():
+    origFirst=""
+    origLast=""
+
+    firstList=["Rusty", "Fiendish","Corky"]
+    lastList=["Swindles","Swabslop","Rattlebones"]
+    def __init__(self,firstName,lastName):
+        x=0
+
+    def CreateName(self):
+        x=0
+
+mypirate=PirateNameGenerator("The Savage Ghost","of the East")
