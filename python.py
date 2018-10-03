d1daeb6a8a016fcc0dd6b8855a06e1434eaa9bcd
tails618 <asher.grossman@gmail.com>
2018-10-02 19:13:02 -0500

    Added words and stuff

--- python.py
+++ python.py
@@ -1,6 +1,19 @@
 import random
 from tkinter import *
 myfont="arial 14 bold"
+def showName():
+    fn=ftext.get()
+    ln=ltext.get()
+    ftext.delete(0,"end")
+    ltext.delete(0,"end")
+    mygen=PirateNameGenerator(fn,ln)
+    pirateOutput=mygen.CreateName()
+    output.config(text=pirateOutput,compound=CENTER)
+
+    '''clear the boxes
+    create and instance of class
+    generate a name
+    show it on screen'''
 
 
 class PirateNameGenerator():
@@ -16,9 +29,7 @@ class PirateNameGenerator():
     def CreateName(self):
         x=random.randint(0,len(self.firstList)-1)
         y=random.randint(0,len(self.lastList)-1)
-        pirateFirst=self.pirateFirstList[randF]
-        pirateLast=self.pirateLastList[randL]
-        return pirateFirst+" "+pirateLast
+        return self.firstList[x]+" "+self.lastList[y]
 
 root=Tk()
 #create controls
@@ -30,8 +41,8 @@ flabel=Label(root,text="First Name",font=myfont)
 llabel=Label(root,text="Last Name",font=myfont)
 ftext=Entry(root,font=myfont)
 ltext=Entry(root,font=myfont)
-bshow=Button(root,text="SHOW ME MY NAME YAAAA!",font=myfont)
-output=Label(root,text="HOI",font="Script",image=banner)
+bshow=Button(root,text="SHOW ME MY NAME YAAAA!",font=myfont,command=showName)
+output=Label(root,text="HOI",font="BlackPearl",image=banner)
 
 #grid controls
 title.grid(row=0,column=0,columnspan=2)
